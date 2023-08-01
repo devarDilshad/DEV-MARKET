6615bd6bef95b35cedd7722080ef841617ece6d2
devarDilshad <devar.dilshad99@gmail.com>
2023-08-01 15:26:34 +0300

    added a new condition

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,23 +5,31 @@ import ChooseUs from "../components/ChooseUs";
 import JoinUs from "../components/JoinUs";
 import { fetchData, options } from "../utils/fetchData";
 
-
 const Home = () => {
-  const [coins,setCoins] = useState([]);
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    const fetchCoins = async() => {
-      const coinsData = await fetchData('https://coinranking1.p.rapidapi.com/coins',options);
+    const fetchCoins = async () => {
+      const coinsData = await fetchData(
+        "https://coinranking1.p.rapidapi.com/coins",
+        options
+      );
       setCoins(coinsData.data.coins);
-    } 
+    };
     fetchCoins();
   }, []);
 
   return (
     <div className="bg-bg_color" id="home">
-      <Hero coins={coins} />
-      <Market coins={coins} />
-      <ChooseUs />
-      <JoinUs />
+      {coins ? (
+        <>
+          <Hero coins={coins} />
+          <Market coins={coins} />
+          <ChooseUs />
+          <JoinUs />
+        </>
+      ) : (
+        "Loading"
+      )}
     </div>
   );
 };
